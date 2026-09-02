be868d7f1e5651a1db084982779f37f43e2c87eb
Arunya <arunyagurjar1@gmail.com>
2026-09-02 23:02:49 +0530

    Initial Commit

---
+++ Xbox Cloud Gaming/Xbox_Cloud_GamingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xbox_Cloud_GamingApp.swift
+//  Xbox Cloud Gaming
+//
+//  Created by Arunya on 02/09/26.
+//
+
+import SwiftUI
+
+@main
+struct Xbox_Cloud_GamingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
